21f034aa714ddc524c9687dd6d758de49e8606bb
Eduardo Moreno Marinho <eduardo_institucional@outlook.com>
2018-08-18 15:29:41 -0300

    Inserção de testes de conflito

--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -2,12 +2,32 @@ package br.com.caelum.ingresso.model;
 
 import java.time.LocalTime;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class Sessao {
 
+	@Id
+	@GeneratedValue
 	private Integer id;
 	private LocalTime horario;
+	
+	@ManyToOne
 	private Sala sala;
+	
+	@ManyToOne
 	private Filme filme;
+	
+	/**
+	*	@deprecated	hibernate	only
+	*/
+	
+	public Sessao(){
+		
+	}
 	
 	public Sessao(LocalTime horario, Filme filme, Sala sala){
 		this.horario = horario;
@@ -34,4 +54,22 @@ public class Sessao {
 	public void setSala(Sala sala) {
 		this.sala = sala;
 	}
+
+	public LocalTime getHorario() {
+		return horario;
+	}
+
+	public void setHorario(LocalTime horario) {
+		this.horario = horario;
+	}
+
+	public Filme getFilme() {
+		return filme;
+	}
+
+	public void setFilme(Filme filme) {
+		this.filme = filme;
+	}
+	
+	
 }
